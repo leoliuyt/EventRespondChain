70eb3bc96a2449070f9891a2fabc2cf3ec59b880
leoliu <leoliuyt@sina.cn>
2016-01-23 22:53:09 +0800

    Initial Commit

---
+++ EventRespondChain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventRespondChain
+//
+//  Created by leoliu on 16/1/23.
+//  Copyright © 2016年 leoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EventRespondChain/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EventRespondChain
+//
+//  Created by leoliu on 16/1/23.
+//  Copyright © 2016年 leoliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
